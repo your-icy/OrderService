d4f0a4d39da93e947a4c15ba9b8c98c4c6e7fcdd
your-icy <dev.harshpal@gmail.com>
2024-06-28 20:19:11 +0530

    "06/28/2024 - payment service integration"

--- src/main/java/com/icycodes/orderservice/exceptions/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/icycodes/orderservice/exceptions/RestResponseEntityExceptionHandler.java
@@ -1,17 +1,21 @@
 package com.icycodes.orderservice.exceptions;
 
 
+import com.icycodes.orderservice.model.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-//    @ExceptionHandler(ProductNotFoundException.class)
-//    public ResponseEntity<ErrorResponse> handleProductNotFoundException(ProductNotFoundException exception){
-//        return new ResponseEntity<>(ErrorResponse.builder()
-//                .errorCode(exception.getErrorCode())
-//                .errorMessage(exception.getMessage())
-//                .build(), HttpStatus.NOT_FOUND);
-//    }
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> handleProductNotFoundException(CustomException exception){
+        return new ResponseEntity<>(ErrorResponse.builder()
+                .errorCode(exception.getErrorCode())
+                .errorMessage(exception.getMessage())
+                .build(), HttpStatus.valueOf(exception.getStatus()));
+    }
 }
