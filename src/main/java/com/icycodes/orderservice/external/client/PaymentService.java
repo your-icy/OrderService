097b277a843641adb1bc585eb11e6de7314eb40b
your-icy <dev.harshpal@gmail.com>
2024-06-29 04:59:44 +0530

    "06/29/2024 - added Circuit Breaker"

--- src/main/java/com/icycodes/orderservice/external/client/PaymentService.java
+++ src/main/java/com/icycodes/orderservice/external/client/PaymentService.java
@@ -1,16 +1,27 @@
 package com.icycodes.orderservice.external.client;
 
+import com.icycodes.orderservice.exceptions.CustomException;
 import com.icycodes.orderservice.external.request.PaymentRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PaymentService/pay")
 public interface PaymentService {
 
     @PostMapping
     public ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest);
+
+    default void fallback(){
+        throw new CustomException(
+                "Payment Service is down",
+                "NOT_AVAILABLE",
+                500
+        );
+    }
 
 
 }
--- src/main/java/com/icycodes/orderservice/external/client/ProductService.java
+++ src/main/java/com/icycodes/orderservice/external/client/ProductService.java
@@ -1,11 +1,14 @@
 package com.icycodes.orderservice.external.client;
 
+import com.icycodes.orderservice.exceptions.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "ProductService/product/")
 public interface ProductService {
 
@@ -14,4 +17,12 @@ public interface ProductService {
             @PathVariable("id") Long productId,
             @RequestParam Long quantity);
 
+
+    default void fallback(Exception exception) {
+        throw new CustomException(
+                "Product Service is down",
+                "NOT_AVAILABLE",
+                500
+        );
+    }
 }
